129a0a49260af89bd90310a004b173194d548aaa
Melanie Panem <melanie.panem@creditone.com>
2020-05-12 14:44:54 -0700

    Revert "swapped out images for desktop and mobile for better ux"
    This reverts commit d0c7ccde42bf9cf787bd3243de8c02d8da2e3c26.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,33 +33,23 @@ function includeHTML() {
 var slide1 = document.getElementById('slide1');
 var slide2 = document.getElementById('slide2');
 var slide3 = document.getElementById('slide3');
-var slide4 = document.getElementById('slide4');
 var thumbslide1 = document.getElementById('thumbslide1');
 var thumbslide2 = document.getElementById('thumbslide2');
 var thumbslide3 = document.getElementById('thumbslide3');
-var thumbslide4 = document.getElementById('thumbslide4');
 
 setInterval(function(){
   if (slide1.classList.contains('active')) {
     thumbslide1.style.opacity = 1;
     thumbslide2.style.opacity = 0.7;
     thumbslide3.style.opacity = 0.7;
-    thumbslide4.style.opacity = 0.7;
   } else if (slide2.classList.contains('active')) {
     thumbslide2.style.opacity = 1;
     thumbslide1.style.opacity = 0.7;
     thumbslide3.style.opacity = 0.7;
-    thumbslide4.style.opacity = 0.7;
   } else if (slide3.classList.contains('active')) {
     thumbslide3.style.opacity = 1;
     thumbslide1.style.opacity = 0.7;
     thumbslide2.style.opacity = 0.7;
-    thumbslide4.style.opacity = 0.7;
-  } else if (slide4.classList.contains('active')) {
-    thumbslide4.style.opacity = 1;
-    thumbslide1.style.opacity = 0.7;
-    thumbslide2.style.opacity = 0.7;
-    thumbslide3.style.opacity = 0.7;
   }
 }, 450);
 
@@ -69,42 +59,25 @@ function chooseImg1() {
   slide1.classList.add('active');
   slide2.classList.remove('active');
   slide3.classList.remove('active');
-  slide4.classList.remove('active');
   thumbslide1.style.opacity = 1;
   thumbslide2.style.opacity = 0.7;
   thumbslide3.style.opacity = 0.7;
-  thumbslide4.style.opacity = 0.7;
 }
 function chooseImg2() {
   slide2.classList.remove('active');
   slide2.classList.add('active');
   slide1.classList.remove('active');
   slide3.classList.remove('active');
-  slide4.classList.remove('active');
   thumbslide2.style.opacity = 1;
   thumbslide1.style.opacity = 0.7;
   thumbslide3.style.opacity = 0.7;
-  thumbslide4.style.opacity = 0.7;
 }
 function chooseImg3() {
   slide3.classList.remove('active');
   slide3.classList.add('active');
   slide1.classList.remove('active');
   slide2.classList.remove('active');
-  slide4.classList.remove('active');
   thumbslide3.style.opacity = 1;
   thumbslide1.style.opacity = 0.7;
   thumbslide2.style.opacity = 0.7;
-  thumbslide4.style.opacity = 0.7;
-}
-function chooseImg4() {
-  slide4.classList.remove('active');
-  slide4.classList.add('active');
-  slide1.classList.remove('active');
-  slide2.classList.remove('active');
-  slide3.classList.remove('active');
-  thumbslide4.style.opacity = 1;
-  thumbslide1.style.opacity = 0.7;
-  thumbslide2.style.opacity = 0.7;
-  thumbslide3.style.opacity = 0.7;
 }
