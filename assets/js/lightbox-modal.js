6763404e959e956b7b49adf0fb33b2ff76996dcc
Melanie Panem <melanie.panem@creditone.com>
2020-05-15 20:02:37 -0700

    removing food lightbox JS

--- assets/js/lightbox-modal.js
+++ assets/js/lightbox-modal.js
@@ -1,55 +1,6 @@
 /* Image Lightbox Modal JS Code */
 
 
-
-
-// FOOD/ABOUT SPECIFIC
-
-// Open the Modal
-function openModalFood() {
-  document.getElementById("myModalFood").style.display = "block";
-}
-
-// Close the Modal
-function closeModalFood() {
-  document.getElementById("myModalFood").style.display = "none";
-}
-
-var slideIndexFood = 1;
-showSlidesFood(slideIndexFood);
-
-// Next/previous controls
-function plusSlidesFood(n) {
-  showSlidesFood(slideIndexFood += n);
-}
-
-// Thumbnail image controls
-function currentSlideFood(n) {
-  showSlidesFood(slideIndexFood = n);
-}
-
-function showSlidesFood(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlidesFood");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndexFood = 1}
-  if (n < 1) {slideIndexFood = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndexFood-1].style.display = "block";
-  dots[slideIndexFood-1].className += " active";
-  captionText.innerHTML = dots[slideIndexFood-1].alt;
-}
-
-
-
-
-
 // MENU SPECIFIC
 
 // Open the Modal
